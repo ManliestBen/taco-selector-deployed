3fb2c10f960275b553d38703e74aabfe4564461b
ManliestBen <benjamintmanley@gmail.com>
2020-09-22 10:45:49 -0500

    Updates

---
+++ precache-manifest.c65e16e7d916ffbe40c7055f2566ce84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1866cfe91c7619388f378ee6c83a8540",
+    "url": "/taco-selector-deployed/index.html"
+  },
+  {
+    "revision": "d7fa96885e4ed7a43ac4",
+    "url": "/taco-selector-deployed/static/css/main.b6f34131.chunk.css"
+  },
+  {
+    "revision": "c02fb3c7e49ead74a6c2",
+    "url": "/taco-selector-deployed/static/js/2.afe85b7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taco-selector-deployed/static/js/2.afe85b7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7fa96885e4ed7a43ac4",
+    "url": "/taco-selector-deployed/static/js/main.a5f9ff16.chunk.js"
+  },
+  {
+    "revision": "aab88f9eaab419524f4a",
+    "url": "/taco-selector-deployed/static/js/runtime-main.1b170d98.js"
+  }
+]);
